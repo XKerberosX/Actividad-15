af6b0085dbcde773dc2915476a37717e674560db
Sergio <a281854@uach.mx>
2020-10-15 11:38:31 -0600

    master
    actividad Completa

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const lista = document.getElementById("lista");
 const url = "https://jsonplaceholder.typicode.com/photos";
 
-fetch(url)
-.then(Response =>  Response.json())
-.then (Posts => {
-    Posts.map(elemento =>{
+//fetch(url)
+//.then(Response =>  Response.json())
+//.then (Posts => {
+    //Posts.map(elemento =>{
+        //var li = document.createElement('li');
+      //  li.innerHTML =`<a href ="detalle.html?id=${elemento.id}">${elemento.id}</a> ${elemento.title}`
+    //    lista.appendChild(li);
+  //  })
+//})
+
+const getPosts = async () =>{
+     const Response = await fetch(url);
+     const Posts = await Response.json();
+     Posts.map(elemento =>{
         var li = document.createElement('li');
         li.innerHTML =`<a href ="detalle.html?id=${elemento.id}">${elemento.id}</a> ${elemento.title}`
         lista.appendChild(li);
     })
-})
+    return Posts;
+}
+getPosts();
+
